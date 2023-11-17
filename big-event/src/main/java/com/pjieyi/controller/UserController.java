d3e0443a05e8e5dc02ff29abe02d661344ce6e82
jieyi123 <pjieyi@foxmail.com>
2023-11-17 21:43:54 +0800

    validated进行参数校验

--- big-event/src/main/java/com/pjieyi/controller/UserController.java
+++ big-event/src/main/java/com/pjieyi/controller/UserController.java
@@ -4,8 +4,10 @@ package com.pjieyi.controller;
 import com.pjieyi.pojo.Result;
 import com.pjieyi.pojo.User;
 import com.pjieyi.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping("/user")
 @RestController
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username,String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}") String username, @Pattern(regexp = "^\\S{5,16}") String password){
+        //  /S{5,16}正则表达式 匹配不为空的5-16个字符
         User user = userService.findByUsername(username);
         if (user == null){
             //用户不存在 新增
