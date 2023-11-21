c5e2e9d693b8461722e6c4355104b726d620604f
jieyi123 <pjieyi@foxmail.com>
2023-11-21 21:39:05 +0800

    文件上传到本地

---
+++ big-event/src/main/java/com/pjieyi/controller/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.pjieyi.controller;
+
+import com.pjieyi.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * @Author pjieyi
+ * @Description 文件上传
+ */
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws IOException {
+        //获取原始文件名
+        String filename=file.getOriginalFilename();
+        filename= UUID.randomUUID()+filename.substring(filename.lastIndexOf("."));
+        file.transferTo(new File("C:\\Users\\pjy17\\Desktop\\img\\"+filename));
+        return Result.success(filename);
+    }
+}
