5eee929e975708785e5f050dc0d9618a8920d420
jieyi123 <pjieyi@foxmail.com>
2023-12-02 15:59:30 +0800

    前端项目初始化

---
+++ big-event-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  vue项目
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ big-event-vue/src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/dist/locale/zh-cn.js'
+const app=createApp(App)
+app.use(ElementPlus,{locale})
+app.mount('#app')
